5bf899ad77ad46c1f8af52f3ec47c842cf562fb7
Che Fernandez <135030757+CheFernandez99@users.noreply.github.com>
2023-12-28 14:53:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const share = document.querySelector('.shareDiv');
-const icon = document.querySelector('#shareicon');
+const icon = document.querySelector('#shareIcon');
  
 share.style.display='none';
 
 icon.addEventListener('click',()=>{
  
     if(share.style.display==='none'){
-      share.style.display='block'
+      share.style.display='block';
+     icon.style.color='white';
     }else{
-      share.style.display='none'
+      share.style.display='none';
+       icon.style.color='';
     }
 
 });
